eb665b7455b785261689e5da69050b4df366eaef
Ste-C <glissando.sc@gmail.com>
2023-11-21 10:04:29 -0600

    Added more.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var map = L.map('map').setView([51.50008, -0.09], 13);
 
 var marker = L.marker([51.50008, -0.09]).addTo(map);
+var marker = L.marker([51.499745, -0.090199]).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
@@ -16,7 +17,8 @@ var latlngs = [
   [51.49985, -0.09108],
   [51.49961, -0.09091],
   [51.499565, -0.090865],
-  [51.49975, -0.090199],
+  [51.4996, -0.09075],
+  [51.499745, -0.090199],
 ];
 
 var polyline = L.polyline(latlngs, {color: 'blue'}).addTo(map);
