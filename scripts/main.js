f69bad14d09292fa8561fe449ab7441ab69d449f
Ste-C <glissando.sc@gmail.com>
2023-11-21 10:47:47 -0600

    Dummy button.

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,31 @@
 var map = L.map('map').setView([51.50008, -0.09], 13);
 
-var marker = L.marker([51.50008, -0.09]).addTo(map);
-var marker = L.marker([51.499745, -0.090199]).addTo(map);
+var marker = L.marker([51.50008, -0.09]).addTo(map);        // Start.
+var marker = L.marker([51.499745, -0.090199]).addTo(map);   // End.
 
+// Tile layers.
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http:www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// L.Control.textbox = L.Control.extend({
+//   onAdd: function(map) {
+    
+//   var text = L.DomUtil.create('div');
+//   text.id = "info_text";
+//   text.innerHTML = "<strong>Add Route</strong>"
+//   return text;
+//   },
+
+//   onRemove: function(map) {
+//     // Nothing to do here
+//   }
+// });
+// L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+// L.control.textbox({ position: 'bottomleft' }).addTo(map);
+
 
 // Create a polyline from an array of LatLng points.
 var latlngs = [
@@ -21,7 +40,29 @@ var latlngs = [
   [51.499745, -0.090199],
 ];
 
+// Adds route to map (use re-draw when updating values in real time).
 var polyline = L.polyline(latlngs, {color: 'blue', fillOpacity:0.2}).addTo(map);
 
 // Zoom the map to the polyline.
 map.fitBounds(polyline.getBounds());
+
+
+// Map pop-ups.
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent(e.latlng.toString())
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
+// Buttons.
+const button = document.getElementById("AddRoute");
+
+button.onclick = () => {
+  alert("BOOP");
+};
