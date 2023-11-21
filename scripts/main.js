878a1c0d63b131df839f2ef3fc31ddb38240dded
Ste-C <glissando.sc@gmail.com>
2023-11-21 09:46:49 -0600

    Changed to hosted version of leaflet.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.5, -0.09], 13);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
